7bbf544a1ebc54a7e482ee1428245fb0a75e6f7a
9pm <9pm@kakao.com>
2026-08-24 09:26:09 +0900

    feat: Add `robots.txt` and generate sitemap for improved SEO; update dependencies in `pnpm-lock.yaml`

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,14 @@
 import { defineConfig } from 'astro/config'
 
 import vue from '@astrojs/vue'
+import sitemap from '@astrojs/sitemap'
 
 import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue()],
+  site: 'https://blue-cherry-cookie.web.app',
+  integrations: [vue(), sitemap()],
 
   vite: {
     plugins: [tailwindcss()],
